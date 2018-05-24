6c3a8060826d660adbfab102f8a9364e4ca343cf
yang <y.alex.sheng@gmail.com>
2018-05-24 08:04:58 -0400

    implemented

---
+++ helper.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	// rename use os.Rename
+	//"os"
+)
+
+// return the current dir where you are to run the binary
+func getPWD() (string, error) {
+	dir, err := os.Getwd()
+	return dir, err
+}
+
+// return the dir of binary no matter where you run it
+func getBinaryPath() (string, error) {
+	dir, err := os.Executable()
+	return dir, err
+}
+
+// this is similar to getBianryPath that return bianry dir
+func absBinary() (string, error) {
+	dir, err := filepath.Abs(path.Join(os.Args[0]))
+	return dir, err
+}
+
+// match func check if a file match this pattern
+func match(fileName string) bool {
+	r := regexp.MustCompile("\\s\\(\\d+\\sof\\s\\d+\\)")
+	return r.MatchString(fileName)
+}
+
+func rename(oldPath, newPath string) error {
+	return os.Rename(oldPath, newPath)
+}
+
+func formNewName(oldName string, seq int) string {
+	r := regexp.MustCompile("\\s\\(\\d+\\sof\\s\\d+\\)")
+	suffix := fmt.Sprintf("_%v", seq)
+	replaced := r.ReplaceAllString(oldName, suffix)
+	return replaced
+}
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"strings"
+	// rename use os.Rename
+	"os"
+	// rename use commands
+	_ "os/exec"
+	// check Walk/WalkFunc
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+var dir = flag.String("root", "", "root directory to traverse")
+var stopDir = flag.String("stop", "", "stop dir. If empty, will travese all sub-directories")
+var depth = flag.Int("depth", -1, "traverse depth. negative value will traverse all sub-directories.")
+
+func main() {
+	flag.Parse()
+	current := ""
+	if *dir == "" {
+		fmt.Println("root is empty. start traversing from current dir")
+		var err error
+		current, err = getPWD()
+		must(err)
+	} else {
+		current = *dir
+	}
+	fi, _ := ioutil.ReadDir(current)
+	for _, f := range fi {
+		fmt.Println(f.Name(), f.IsDir())
+	}
+	fmt.Println()
+	fmt.Println("start walking")
+	count := 0
+	err := filepath.Walk(current, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			// fmt.Println("find dir: ", info.Name(), "with path: ", path)
+			// return filepath.SkipDir
+			return nil
+		}
+		fileName := info.Name()
+		if match(fileName) {
+			fmt.Println("find file: ", info.Name(), "with path: ", path)
+			newName := formNewName(fileName, count)
+			newPath := strings.Replace(path, fileName, newName, -1)
+			err := rename(path, newPath)
+			must(err)
+			count++
+		}
+		return nil
+	})
+	must(err)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
